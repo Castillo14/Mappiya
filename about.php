e5954054ced4ddc5e0c44b83e33becbf555bea85
Castillo14 <castillomaryjoy14@gmail.com>
2022-08-09 15:46:46 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>MAPPIYA</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="assets/css/theme.css" rel="stylesheet" />
+
+  </head>
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand" href="index.html"><img src="assets/image/icon.jpg"height="45" alt="logo" /><b>MAPPIYA</b></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
+          <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
+              <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="index.php">Home</a></li>
+              <li class="nav-item px-2"><a class="nav-link" href="about.php">About</a></li>
+              <div class="dropdown">
+          <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">Services</button>
+           <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Padala </a></li>
+            <li><a class="dropdown-item" href="#">Pabili</a></li>
+            <li><a class="dropdown-item" href="#">Pa Groceries</a></li>
+            <li><a class="dropdown-item" href="#">Taxi Services (Pick & Drop)</a></li>
+            <li><a class="dropdown-item" href="#">Cooperive Members</a></li>
+            <li><a class="dropdown-item" href="#">Ofw Discount</a></li>
+            <li><a class="dropdown-item" href="#">Senior Citizen</a></li>
+            <li><a class="dropdown-item" href="#">Booking tour in K.S.A</a></li>
+          </ul>
+            </div>
+        </li>
+         <li class="nav-item px-2"><a class="nav-link" href="#services">Contact</a></li>
+          </ul>
+            <div class="dropdown d-none d-lg-block">
+              <button class="btn bg-soft-warning ms-2" id="dropdownMenuButton1" type="submit" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-search text-warning"></i></button>
+              <div class="dropdown-menu dropdown-menu-lg-end p-0 rounded" aria-labelledby="dropdownMenuButton1" style="top:55px">
+                <form>
+                  <input class="form-control border-200" type="search" placeholder="Search" aria-label="Search" style="background:#FDF1DF;" />
+                </form>
+              </div>
+            </div>
+            <form class="d-flex my-3 d-block d-lg-none">
+              <input class="form-control me-2 border-200 bg-light" type="Track Here" placeholder="Track Here" aria-label="Search" />
+              <button class="btn btn-outline-primary" type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <section class="py-xxl-10 pb-0" id="home">
+        <div class="bg-holder bg-size" style="background-image:url(assets/image/background.jpg);background-position:top center;background-size:cover;">
+        </div>
+        <!--/.bg-holder-->
+
+    
+       <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-5 col-xl-6 col-xxl-7 order-0 order-md-1 text-end"><img class="pt-7 pt-md-0 w-100" src="assets/image/about-owner1.jpg" alt="hero-header" /></div>
+            <div class="col-md-75 col-xl-6 col-xxl-5 text-md-start text-center py-8">
+              <p class="fs-1 mb-5">MAPPIYA is derived from the language of the Maranao Tribe that means it’s okay or good. Mappiya App was developed by YaraMay Computer and Maintenance Services that is based on Manila, Philippines.
+                <a href="#demo" class="btn btn-danger collapsed" data-bs-toggle="collapse" aria-expanded="false">Read More</a>
+            <div id="demo" class="collapse">
+              The Mappiya App was developed to help our Kababayans who have difficulty going out of their homes/workplace to make necessary transactions and purchases elsewhere, whether be it for themselves or their families in K.S.A./the Philippines. Choose from the variety of services that we offer like Padala, Pabili, Guided Tour (Riyadh), and Airport Pickup/Dropoff (KSA and Manila). Mappiya also offers discounts for OFWs, Senior Citizens and  People  tel Cooperative Members. We are also commited to provide 24/7 support to address your problems regarding transactions done through the Mappiya App.
+            </div>
+           </div>
+          </div>
+        </div>
+      </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
+
+
+
+
+
+
+   <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="bg-900 pb-0 pt-5">
+
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0"><a class="text-decoration-none fw-bolder mb-4 font-sans-serif" href="#"><img src="assets/img" height="51" alt="" />MAPPIYA</a>
+              <p class="text-500 my-4">MAPPIYA is derived from the language of the Maranao Tribe that means it’s okay or good. Mappiya App was developed by YaraMay Computer and Maintenance Services that is based on Manila, Philippines..</p>
+            </div>
+            <div class="col-12 col-sm-12 col-lg-3 mb-2 order-0 order-sm-0">
+              <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif">Services</h5>
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="lh-lg"><a class="text-500" href="#!">Padala</a></li>
+                <li class="lh-lg"><a class="text-500" href="#!">Pabili</a></li>
+                <li class="lh-lg"><a class="text-500" href="#!">Pa Groceries</a></li>
+                <li class="lh-lg"><a class="text-500" href="#!">Taxi Services (Pick & Drop)</a></li>
+              </ul>
+            </div>
+            <div class="col-12 col-sm-12 col-lg-3 mb-3 order-0 order-sm-0">
+              <h5 class="lh-lg fw-bold text-light mb-4 font-sans-serif"> Contact Us</h5>
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="lh-lg"><a class="text-500" href="#!"><i class="fa fa-location-arrow" aria-hidden="true"></i>Cluster 5 1g Campbridge Brgy. San Isidro ,Cainta Rizal </a></li>
+                <li class="lh-lg"><a class="text-500" href="#!"><i class="fa fa-phone" aria-hidden="true"></i>00639182550953</a></li>
+                <li class="lh-lg"><a class="text-500" href="#!"><i class="fa fa-phone" aria-hidden="true"></i>00966508624264</a></li>
+                 <li class="lh-lg"><a class="text-500" href="#!"><i class="fa fa-envelope" aria-hidden="true"></i>sab_princes@yahoo.com</a></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="py-0 bg-1000">
+
+        <div class="container">
+          <div class="row justify-content-md-between justify-content-evenly py-4">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
+              <p class="fs--1 my-2 fw-bold text-200">All rights Reserved &copy; MAPPIYA, 2022</p>
+            </div>
+           </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="vendors/@popperjs/popper.min.js"></script>
+    <script src="vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="vendors/is/is.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="vendors/fontawesome/all.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+  </body>
+
+</html>
